8d333e235acc0221710ccacc5d5cca4b565d49c1
Santosh Nainala <51361959+santoshnainala@users.noreply.github.com>
2023-01-06 22:01:24 +0530

    Create Arthematic operator.py

---
+++ Arthematic operator.py
@@ -0,0 +1,14 @@
+x= 8
+y= 4
+
+# Addition of two operands 
+add = x+ y
+print(add) 
+
+# Subtraction of two operands
+sub = x-y
+print (sub)
+
+# Multiplication of two operands
+multiplication = x*y
+print (multiplication)
